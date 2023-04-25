6758e7ead5f288f647d4ffa11c3f1f4bd5dcb457
Tatiana Smirnova <redkiivid@mail.ru>
2023-04-25 20:49:16 +0300

    New commit

--- src/test/java/ru/netology/GameTest.java
+++ src/test/java/ru/netology/GameTest.java
@@ -1,7 +1,7 @@
 package ru.netology;
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import ru.netology.exceptions.NotRegisteredException;
 
